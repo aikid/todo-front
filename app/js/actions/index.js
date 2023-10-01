863f50d4e929b0c076bb2c4218e53ffce0cd6e4f
Leandro Nogueira <leandro.nogueira@goflux.com.br>
2023-10-01 11:17:57 -0300

    inital commit

---
+++ app/js/actions/index.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://97rwbp4ioc.execute-api.us-east-1.amazonaws.com/dev/todos';
+
+if (!API_URL) {
+  console.error('Set `API_URL` in `app/js/actions/index.js` to your deployed endpoint')
+}

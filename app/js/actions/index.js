b406c35b68b66eb5b06303ab316ad1f7435ec862
Leandro Nogueira <leandro.nogueira@goflux.com.br>
2023-10-03 08:51:20 -0300

    change endpoint url

--- app/js/actions/index.js
+++ app/js/actions/index.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://97rwbp4ioc.execute-api.us-east-1.amazonaws.com/dev/todos';
+export const API_URL = 'https://kv15ifr9hd.execute-api.us-east-1.amazonaws.com/dev/todos';
 
 if (!API_URL) {
   console.error('Set `API_URL` in `app/js/actions/index.js` to your deployed endpoint')
